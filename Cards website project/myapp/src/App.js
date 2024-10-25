c6135f3e78b044413459ed7fa533e8df75cbab28
TalhaMudassar <talhamudassar200@gmail.com>
2024-10-26 01:03:51 +0500

    css codes

---
+++ Cards website project/myapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+function App() {
+  return (
+<>
+ <Home />
+ <About />
+</>
+  );
+}
+
+export default App;
